ce05973d1cc3678969f178cd67e065967aa0a2e0
Fan Yang <fanyang@fans-mbp.eeob.iastate.edu>
2016-05-10 08:56:42 -0500

    calculate the diffs and print them into tab delimited format

--- week1_nicole/find_PCR_product_germs_fy.py
+++ week1_nicole/find_PCR_product_germs_fy.py
@@ -1,14 +1,8 @@
-#writing a program to find 500 bp fragments in blast file
-
-#Adina created the blast file and put them in order
-#in R I added a column that gave the difference between two adjacent rows
-
-#need to find the ~500 bp fragments
-#and output both that row and the one before it to get both primers
-
-#originally run on hmp_with_diff file; then mock_imperfect file
-
-#finally on usda mock file but changed to 400-600 (from 300-700)-still hundreds
+# this script recreates the last column in test file (ie "diff")
+# for hits found in the same gene, it subtracts the nth row end position 
+# by (n-1)th row end position to create the fragment length.
+#
+# note: all primer hits have to be already sorted by its hit positions for this code to work. 
 
 import sys
 
@@ -24,11 +18,15 @@ for n, line in enumerate(open(sys.argv[1], 'rU')):
 		d[genome] = [end_pos]
 	else:
 		d[genome].append(end_pos)
-
-
+d2 = {}
 for key in d:
+	diff_col = ["NA"]
 	l = zip(d[key], d[key][1:])
-	print [int(y) - int(x) for x,y in l]
-
-#output=open("/Users/nicolericker/Documents/DARTE-QM/usda_500", "w")
+	for x, y in l:
+		diff = int(y) - int(x) 
+		diff_col.append(diff)
+	new = zip(d[key], diff_col)
+	for i in range(0, len(new)):
+		print '%s\t%s\t%s' % (key, new[i][0], new[i][1]) 
+	
 
